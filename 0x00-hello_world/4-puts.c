1bfac4d4fbbd041e510547d2113d71ca42ba99b1
flawar1 <udofiaidorenyen@gmail.com>
2023-04-20 19:37:08 -0700

    101-quote.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - main entry point
- * return : always 0 (success)
+ * main - main "entry point"
+ * return : 1 if it is positive. 0 otherwise.
  */
 int main(void)
 {
