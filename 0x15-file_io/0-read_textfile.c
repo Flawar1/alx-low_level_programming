3675e244bb8e72e82b0d726630cad083c35bc81a
Flawar1 <udofiaidorenyen@gmail.com>
2023-06-17 10:55:13 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,59 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * read_textfile - reads a text file and prints the letters
  * @filename: filename.
  * @letters: numbers of letters printed.
  *
- * Return: numbers of letters printed. It fails, return 0.
+ * Return: numbers of letters printed. It fails, returns 0.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file;
-	char *buffer;
-	ssize_t bytes_read, bytes_written;
+	int fd;
+	ssize_t rnw, rww;
+	char *buf;
 
-	if (filename == NULL)
-	{
+	if (!filename)
 		return (0);
-	}
 
-	file = fopen(filename, "f");
-	if (file == NULL)
-	{
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
-	}
 
-	buffer = (char *)malloc((letters + 1) * sizeof(char));
-	if (buffer == NULL)
-	{
-		fclose(file);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
 
-	bytes_read = fread(buffer, sizeof(char), letters, file);
-	if (bytes_read <= 0)
-	{
-		fclose(file);
-		free(buffer);
-		return (0);
-	}
+	rnw = read(fd, buf, letters);
+	rww = write(STDOUT_FILENO, buf, rnw);
 
-	buffer[bytes_read] = '\0';
+	close(fd);
 
-	bytes_written = printf("%s", buffer);
+	free(buf);
 
-	fclose(file);
-	free(buffer);
-
-	if (bytes_written <= 0 || bytes_written != bytes_read)
-	{
-		return (0);
-	}
-
-	return (bytes_written);
+	return (rww);
 }
-
-
