ce227a9a2631ff02771146ad968bd600872a4ed3
Flawar1 <udofiaidorenyen@gmail.com>
2023-06-17 09:57:09 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,13 +10,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int nletters;
+	int letters;
 	int fwd;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0700);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
@@ -24,10 +24,10 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++)
+	for (letters = 0; text_content[letters]; letters++)
 		;
 
-	fwd = write(fd, text_content, nletters);
+	fwd = write(fd, text_content, letters);
 
 	if (fwd == -1)
 		return (-1);
